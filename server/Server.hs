5d115dd46b34347e6ff47cf753bc624c30195cad
Colin Woodbury <colingw@gmail.com>
2018-01-26 11:09:01 -0800

    Basic server structure

--- server/Server.hs
+++ server/Server.hs
@@ -1,8 +1,30 @@
-module Main where
+{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-import Protolude
+module Main ( main ) where
+
+import           Data.Proxy
+import qualified Network.Wai.Handler.Warp as W
+import           Options.Generic
+import           Protolude
+import           Servant.API
+import           Servant.Server
 
 ---
 
+data Env = Env { port :: Maybe Int <?> "Port to listen for requests on." } deriving (Generic)
+
+instance ParseRecord Env
+
+type API = "posts" :> Get '[JSON] [Text]
+
+server :: Server API
+server = pure []
+
+app :: Application
+app = serve (Proxy :: Proxy API) server
+
 main :: IO ()
-main = putText "Hello, Miso!!"
+main = do
+  Env (Helpful p) <- getRecord "Backend server for fosskers.ca"
+  W.run (maybe 8081 identity p) app
