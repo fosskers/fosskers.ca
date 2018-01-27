c72e25cb5021a524ddf94375bbd1363d16cb7836
Colin Woodbury <colingw@gmail.com>
2018-01-27 09:38:54 -0800

    `posts` endpoint also returns word frequencies
    - This will help with keyword-based post search

--- server/Server.hs
+++ server/Server.hs
@@ -4,9 +4,14 @@
 
 module Main ( main ) where
 
+import           Control.Arrow ((&&&))
+import           Data.Aeson (ToJSON)
+import           Data.Char (isAlpha)
 import           Data.Default (def)
-import qualified Data.Map.Strict as M
+import qualified Data.HashMap.Strict as M
 import           Data.Proxy
+import qualified Data.Set as S
+import qualified Data.Text as T
 import           Filesystem.Path (basename)
 import qualified Network.Wai.Handler.Warp as W
 import           Options.Generic
@@ -22,16 +27,18 @@ import           Text.Pandoc.Writers.HTML (writeHtml5)
 
 ---
 
-data Args = Args { port :: Maybe Int <?> "Port to listen for requests on." } deriving (Generic, ParseRecord)
+newtype Args = Args { port :: Maybe Int <?> "Port to listen for requests on." } deriving (Generic, ParseRecord)
 
-data Env = Env { posts :: M.Map Text Html }
+newtype Env = Env { posts :: M.HashMap Text (M.HashMap Text Int, Html) }
 
-type API = "posts" :> Get '[JSON] [Text]
+data Blog = Blog { title :: Text, freqs :: M.HashMap Text Int } deriving (Generic, ToJSON)
+
+type API = "posts" :> Get '[JSON] [Blog]
   :<|> "posts" :> Capture "post" Text :> Get '[HTML] Html
 
 server :: Env -> Server API
-server env = pure (M.keys $ posts env)
-  :<|> (\f -> maybe (throwError err404) pure . M.lookup f $ posts env)
+server env = pure (blogs env)
+  :<|> (\f -> maybe (throwError err404) (pure . snd) . M.lookup f $ posts env)
 
 app :: Env -> Application
 app = serve (Proxy :: Proxy API) . server
@@ -41,11 +48,21 @@ html :: Text -> Maybe Html
 html t = either (const Nothing) Just $ runPure h
   where h = readOrg def t >>= writeHtml5 def
 
+-- | A mapping of word frequencies.
+freq :: Text -> M.HashMap Text Int
+freq = M.fromList . map ((maybe "死毒殺悪厄魔" T.toLower . head) &&& length) . group . sort . filter p . T.words . T.map f
+  where f c | isAlpha c = c
+            | otherwise = ' '
+        p = uncurry (&&) . ((> 2) &&& (< 13)) . T.length
+
+blogs :: Env -> [Blog]
+blogs = map (\(t, (m, _)) -> Blog t m) . M.toList . posts
+
 main :: IO ()
 main = do
   Args (Helpful p) <- getRecord "Backend server for fosskers.ca"
   ps <- shelly (ls "blog" >>= traverse (\f -> (toTextIgnore $ basename f,) <$> readfile f))
   let prt = maybe 8081 identity p
-      ps' = M.fromList . catMaybes $ map (traverse html) ps
+      ps' = M.fromList . catMaybes $ map (traverse (sequenceA . (freq &&& html))) ps
   putText $ "Running on port " <> show prt
   W.run prt . app $ Env ps'
