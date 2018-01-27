583324562a89065991a57bc7195e7e6e651c77a0
Colin Woodbury <colingw@gmail.com>
2018-01-27 13:18:38 -0800

    Generate Purescript types
    - The query code doesn't quite work yet.

--- bridge/Bridge.hs
+++ bridge/Bridge.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE DataKinds #-}
+
 module Main where
 
+import Language.PureScript.Bridge
 import Protolude
 import Servant.PureScript
+import Types
 
 ---
 
+types :: [SumType 'Haskell]
+types = [ mkSumType (Proxy :: Proxy Blog) ]
+
 main :: IO ()
-main = pure ()
+-- main = writeAPIModule "." _ (Proxy :: Proxy API)
+main = writePSTypes "frontend" (buildBridge defaultBridge) types
