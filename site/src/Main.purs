88e17ebf5e85fe027adb0263421103fae94df7eb
Colin Woodbury <colingw@gmail.com>
2018-01-29 18:35:21 -0800

    Basic halogen example

--- site/src/Main.purs
+++ site/src/Main.purs
@@ -1,9 +1,31 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Control.Monad.Eff (Eff)
+import Data.Identity (Identity(..))
+import Data.Maybe (Maybe(..))
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.VDom.Driver (runUI)
+
+---
+
+message :: forall a. H.Component HH.HTML Identity Unit Unit a
+message = H.component { initialState: const unit
+                      , render
+                      , eval
+                      , receiver: const Nothing }
+
+render :: forall q s. s -> H.ComponentHTML q
+render _ = HH.text "hi"
+
+eval :: forall s a. Identity ~> H.ComponentDSL s Identity Unit a
+eval = case _ of
+  Identity next -> pure next
+
+main :: Eff (HA.HalogenEffects ()) Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI message unit body
