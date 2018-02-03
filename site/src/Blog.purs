cf7803dbe151fb0cb07f30f4b97621cad71b6ab2
Colin Woodbury <colingw@gmail.com>
2018-02-03 10:17:36 -0800

    Proper language switching for blog posts

--- site/src/Blog.purs
+++ site/src/Blog.purs
@@ -4,12 +4,13 @@ import Prelude
 
 import Data.Maybe (Maybe(..), maybe)
 import Data.Monoid (mempty)
+import Data.Tuple (Tuple(..))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Search as Search
-import Types (Language(English))
+import Types (Language(..))
 
 ---
 
@@ -21,20 +22,34 @@ data Slot = SearchSlot
 derive instance eqSlot  :: Eq Slot
 derive instance ordSlot :: Ord Slot
 
-type Post = { title :: String, filename :: String }
+type Post = { engTitle :: String, japTitle :: String, filename :: String }
 
 component :: forall m. H.Component HH.HTML Query Unit Void m
-component = H.parentComponent { initialState: const { language: English , options: mempty , selected: Nothing }
+component = H.parentComponent { initialState: const { language: English
+                                                    , options: [ { engTitle: "Nix and Dante"
+                                                                 , japTitle: "NixとDante"
+                                                                 , filename: "nix" }]
+                                                    , selected: Nothing }
                               , render
                               , eval
                               , receiver: const Nothing }
 
 render :: forall m. State -> H.ParentHTML Query Search.Query Slot m
-render state = HH.div_ $ [ search ] <> choices <> [ post ]
-  where search  = HH.div_ [ HH.slot SearchSlot Search.component state.language (HE.input NewKeywords) ]
-        post    = maybe (HH.div_ []) (\s -> HH.iframe [ HP.src $ "../blog/" <> s <> ".html" ]) state.selected
-        choices = map f state.options
-        f p     = HH.a [ HP.href "#", HE.onClick $ const (Just $ Selected p.filename unit) ] [ HH.h3_ [ HH.text p.title ] ]
+render s = HH.div_ $ [ search ] <> choices s <> [ post s ]
+  where search = HH.div_ [ HH.slot SearchSlot Search.component s.language (HE.input NewKeywords) ]
+
+post :: forall c q. State -> HH.HTML c q
+post state = maybe (HH.div_ []) (\s -> HH.iframe [ HP.src $ "../blog/" <> s <> postfix <> ".html" ]) state.selected
+  where postfix = case state.language of
+          English  -> ""
+          Japanese -> "-jp"
+
+choices :: forall c. State -> Array (HH.HTML c (Query Unit))
+choices state = map f state.options
+  where f p = let title = case state.language of
+                    English  -> p.engTitle
+                    Japanese -> p.japTitle
+              in HH.a [ HP.href "#", HE.onClick $ const (Just $ Selected p.filename unit) ] [ HH.h3_ [ HH.text title ] ]
 
 eval :: forall m. Query ~> H.ParentDSL State Query Search.Query Slot Void m
 eval = case _ of
