6733ec2dc822c8d9ee6eaefc00c57f1bd5bfaa72
Colin Woodbury <colingw@gmail.com>
2018-02-20 11:04:01 -0800

    (site) Completed Japanese version of `About` page

--- site/src/About.purs
+++ site/src/About.purs
@@ -1,11 +1,33 @@
-module About where
+module About ( about ) where
 
 import Prelude
 
+import Bootstrap (col_, container, row_)
+import CSS (paddingTop, pct)
 import Halogen.HTML as HH
+import Halogen.HTML.CSS as HC
 import Halogen.HTML.Properties as HP
+import Types (Language(..))
 
 ---
+
+-- | Dealing with the official 3-Tuples is annoying.
+data Three a b c = Three a b c
+
+about :: forall c q. Language -> HH.HTML c q
+about l = container [HC.style <<< paddingTop $ pct 1.0]
+        [ row_ [ col_ a ]
+        , row_ [ col_ [ row_ [ col_ [ HH.img [ HP.src "assets/jack.jpg" ]]]
+                      , row_ [ col_ [ HH.i_ [ HH.text j ]]]
+                      ]
+               , col_ [ row_ [ col_ [ HH.img [ HP.src "assets/qtip.jpg" ]]]
+                      , row_ [ col_ [ HH.i_ [ HH.text q ]]]
+                      ]
+               ]
+        ]
+  where Three a j q = case l of
+          English  -> Three english "Jack in a sunbeam." "Qtip mentoring me on Category Theory."
+          Japanese -> Three japanese "日差しに耐えられないジャック" "キューチップ博士は圏論の教鞭を執る"
 
 english :: forall c q. Array (HH.HTML c q)
 english = [ HH.p_
@@ -17,14 +39,27 @@ english = [ HH.p_
               <> " Functional programming interests me greatly and allows me to realize my core"
               <> " programming values: Rigour, Minimalism, and Art."
             ]
-          , HH.h3_ [ HH.text "Projects of Note" ]
+          , HH.h3_ [ HH.text "Public Projects of Note" ]
           , HH.ul_
-            [ HH.li_ [ HH.text "The Aura Package Manager for Arch Linux" ]
-            , HH.li_ [ HH.text "Mapbox VectorTile codecs (Haskell, Scala)" ]
-            , HH.li_ [ HH.text "VectorPipe (author)" ]
-            , HH.li_ [ HH.text "GeoTrellis (code contributor and docs author)" ]
-            , HH.li_ [ HH.text "ScalaZ and Cats Comparison" ]
-            , HH.li_ [ HH.text "Scala Benchmarks" ]
+            [ HH.li_ [ HH.text "The "
+                     , HH.a [ HP.href "https://github.com/aurapm/aura" ] [ HH.text "Aura Package Manager" ]
+                     , HH.text " for Arch Linux" ]
+            , HH.li_ [ HH.text "Mapbox VectorTile codecs ("
+                     , HH.a [ HP.href "https://github.com/fosskers/vectortiles" ] [ HH.text "Haskell" ]
+                     , HH.text ", "
+                     , HH.a [ HP.href "https://github.com/locationtech/geotrellis/tree/master/vectortile" ]
+                       [ HH.text "Scala" ]
+                       , HH.text ")" ]
+            , HH.li_ [ HH.a [ HP.href "https://geotrellis.github.io/vectorpipe/" ] [ HH.text "VectorPipe" ]
+                     , HH.text " (author)" ]
+            , HH.li_ [ HH.a [ HP.href "https://github.com/locationtech/geotrellis" ] [ HH.text "GeoTrellis"]
+                     , HH.text " (code contributor and "
+                     , HH.a [ HP.href "https://docs.geotrellis.io/en/latest/" ] [ HH.text "docs author" ]
+                     , HH.text ")" ]
+            , HH.li_ [ HH.a [ HP.href "https://github.com/fosskers/scalaz-and-cats" ]
+                       [ HH.text "ScalaZ and Cats Comparison" ]]
+            , HH.li_ [ HH.a [ HP.href "https://github.com/fosskers/scala-benchmarks"]
+                       [ HH.text "Scala Collections Benchmarks" ]]
             ]
           , HH.p_
             [ HH.text $ "Otherwise, I've written code in many languages including Purescript, Elm, Racket, "
@@ -32,7 +67,7 @@ english = [ HH.p_
               <> " a variety of industry-standard tools and frameworks." ]
           , HH.p_
             [ HH.text "For more specifics, please see "
-            , HH.a [HP.href "https://stackoverflow.com/cv/colinwoodbury"] [ HH.text "my CV."]]
+            , HH.a [ HP.href "https://stackoverflow.com/cv/colinwoodbury"] [ HH.text "my CV."]]
           , HH.p_
             [ HH.text $ "Before pursuing programming professionally, I taught English in Japan for several years."
             <> " I'm fluent in both English and Japanese, speak German at an intermediate level,"
@@ -41,5 +76,47 @@ english = [ HH.p_
             <> " Jack and Qtip." ]
           ]
 
--- Academic Field experience
--- Recommended novels
+japanese :: forall c q. Array (HH.HTML c q)
+japanese = [ HH.p_
+             [ HH.text $ "ウッドブリ・コリンと申します。ネット上では「fosskers」という通称で知られています。"
+               <> "基本、Haskellでプログラミングをやっているが前にもScala開発の仕事をしていました。"
+               <> "作り上げた"
+             , HH.a [ HP.href "http://hackage.haskell.org/user/fosskers" ]
+               [ HH.text "librariesは複数"]
+             , HH.text $ "でHaskellとScala両方のコミュニティに活躍しています。"
+               <> "私には関数型プログラミング (Functional Programming) はとても興味深く、FPを通して己れの基本的"
+               <> "開発美徳の三つが叶う：　厳密・最小主義・芸術"
+             ]
+           , HH.h3_ [ HH.text "主な公開プロジェクト" ]
+           , HH.ul_
+             [ HH.li_ [ HH.text "Arch Linuxの"
+                      , HH.a [ HP.href "https://github.com/aurapm/aura" ] [ HH.text "Aura Package Manager" ]]
+             , HH.li_ [ HH.text "Mapbox VectorTileコーデック ("
+                      , HH.a [ HP.href "https://github.com/fosskers/vectortiles" ] [ HH.text "Haskell" ]
+                      , HH.text ", "
+                      , HH.a [ HP.href "https://github.com/locationtech/geotrellis/tree/master/vectortile" ]
+                        [ HH.text "Scala" ]
+                      , HH.text ")" ]
+             , HH.li_ [ HH.a [ HP.href "https://geotrellis.github.io/vectorpipe/" ] [ HH.text "VectorPipe" ]]
+             , HH.li_ [ HH.a [ HP.href "https://github.com/locationtech/geotrellis" ] [ HH.text "GeoTrellis"]
+                      , HH.text " （コードの寄付・"
+                      , HH.a [ HP.href "https://docs.geotrellis.io/en/latest/" ] [ HH.text "docsの創作" ]
+                      , HH.text "）" ]
+             , HH.li_ [ HH.a [ HP.href "https://github.com/fosskers/scalaz-and-cats" ]
+                        [ HH.text "ScalaZとCatsの比べ" ]]
+             , HH.li_ [ HH.a [ HP.href "https://github.com/fosskers/scala-benchmarks"]
+                        [ HH.text "Scala Collections Benchmarks" ]]
+             ]
+           , HH.p_
+             [ HH.text $ "他にはPurescript・Elm・Racket・C・Pythonを含めて様々なプログラミング言語で開発した経験もあります。"
+               <> "AWS・Digital Ocean・Herokuなどにプロジェクト展開した事も。"]
+           , HH.p_
+             [ HH.text "職歴の"
+             , HH.a [ HP.href "https://stackoverflow.com/cv/colinwoodbury"] [ HH.text "詳細はこちら"]
+             , HH.text "。"]
+           , HH.p_
+             [ HH.text $ "開発者になる前、数年間長崎県の小中学校で英語教師として務めていました。英語・日本語を対等に、"
+               <> "ドイツ語を中級で"
+               <> "イタリア語を初級で会話ができます。言語学習の他、自由時間でボルダリングをよくやっています。"
+               <> "彼女とうちの猫二匹と暮らしています。"]
+           ]
