73d9dddd4de0f040282fd4ff3417b63ac622f625
Colin Woodbury <colingw@gmail.com>
2018-03-05 11:50:45 -0800

    (server) New `XML` content type
    - If this works, I'll break it out into its own library: `servant-xml`

---
+++ lib/Fosskers/XML.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
+module Fosskers.XML where
+
+import           Data.ByteString.Builder (toLazyByteString)
+import qualified Network.HTTP.Media as M
+import           Protolude
+import           Servant.API
+import           Xmlbf
+
+---
+
+data XML
+
+instance Accept XML where
+  contentType _ = "text" M.// "xml" M./: ("charset", "utf-8")
+
+instance ToXml a => MimeRender XML a where
+  mimeRender _ = toLazyByteString . encode . toXml
