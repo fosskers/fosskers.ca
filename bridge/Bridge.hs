398af21a8d5de86d7e3cfa4226fe2b569474c81a
Colin Woodbury <colingw@gmail.com>
2018-03-13 13:52:10 -0700

    (bridge) Bridge over all the Kanji types

--- bridge/Bridge.hs
+++ bridge/Bridge.hs
@@ -4,9 +4,12 @@
 module Main ( main ) where
 
 import ClassyPrelude
+import Data.Kanji.Types
 import Data.Proxy (Proxy(..))
 import Fosskers.Common
+import Fosskers.Kanji
 import Language.PureScript.Bridge
+import Language.PureScript.Bridge.PSTypes
 import Servant.PureScript
 import Time.Types
 
@@ -21,14 +24,29 @@ psMonth = TypeInfo { _typePackage    = "purescript-datetime"
 monthBridge :: BridgePart
 monthBridge = typeName ^== "Month" >> pure psMonth
 
+floatBridge :: BridgePart
+floatBridge = typeName ^== "Float" >> pure psNumber
+
+charBridge :: BridgePart
+charBridge = typeName ^== "Char" >> pure psChar
+
+psChar :: PSType
+psChar = TypeInfo { _typePackage    = "purescript-prim"
+                  , _typeModule     = "Prim"
+                  , _typeName       = "Char"
+                  , _typeParameters = [] }
+
 -- | Generic representations of the Haskell types I want to convert to Purescript.
 types :: [SumType 'Haskell]
 types = [ mkSumType (Proxy :: Proxy Blog)
         , mkSumType (Proxy :: Proxy Title)
         , mkSumType (Proxy :: Proxy Date)
-        , mkSumType (Proxy :: Proxy Path) ]
+        , mkSumType (Proxy :: Proxy Path)
+        , mkSumType (Proxy :: Proxy Analysis)
+        , mkSumType (Proxy :: Proxy Kanji)
+        , mkSumType (Proxy :: Proxy Level) ]
 
 main :: IO ()
 main = do
-  writePSTypes "site/src" (buildBridge $ defaultBridge <|> monthBridge) types
+  writePSTypes "site/src" (buildBridge $ defaultBridge <|> monthBridge <|> floatBridge <|> charBridge) types
   writeAPIModule "site/src" defaultBridgeProxy (Proxy :: Proxy JsonAPI)
--- lib/Fosskers/Kanji.hs
+++ lib/Fosskers/Kanji.hs
@@ -5,22 +5,24 @@ module Fosskers.Kanji where
 import           ClassyPrelude
 import           Data.Aeson
 import           Data.Kanji
+import qualified Data.Map.Strict as M
 import qualified Data.Set as S
 import qualified Data.Text as T
 
 ---
 
-data Analysis = Analysis { levelSplit    :: Map Level (Set Kanji)  -- ^ Unique `Kanji` that appeared in each `Level`.
+data Analysis = Analysis { levelSplit    :: [(Level, [Kanji])]  -- ^ Unique `Kanji` that appeared in each `Level`.
                          , elementary    :: Float  -- ^ Percent (<= 1.0) of `Kanji` learned in Elementary school.
                          , average       :: Float  -- ^ Average `Level` of all `Kanji` present.
                          , density       :: Float  -- ^ Fraction (<= 1.0) of the input `Char`s that were `Kanji`.
-                         , unknowns      :: Set Kanji  -- ^ `Kanji` whose `Level` can't be determined.
-                         , distributions :: Map Level Float  -- ^ Fractions of input `Kanji` that belong to each `Level`.
+                         , unknowns      :: [Kanji]  -- ^ `Kanji` whose `Level` can't be determined.
+                         , distributions :: [(Level, Float)]  -- ^ Fractions of input `Kanji` that belong to each `Level`.
                          } deriving (Generic, ToJSON)
 
 analysis :: Text -> Analysis
-analysis t = Analysis (uniques ks) (elementaryDen dist) (averageLevel ks) den uns dist
+analysis t = Analysis uniq (elementaryDen dist) (averageLevel ks) den uns (M.toList dist)
   where ks   = mapMaybe kanji $ unpack t
+        uniq = M.toList . fmap S.toList $ uniques ks
         dist = levelDist ks
-        uns  = S.filter (isNothing . level) $ S.fromList ks
+        uns  = S.toList . S.filter (isNothing . level) $ S.fromList ks
         den  = kanjiDensity (T.length t) ks
