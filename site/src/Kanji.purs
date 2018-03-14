d7f1d1f9cc0a41ece4ce085e24ad27f57e00e8d4
Colin Woodbury <colingw@gmail.com>
2018-03-14 14:12:21 -0700

    (site) Use `POST` to call backend

--- site/src/Kanji.purs
+++ site/src/Kanji.purs
@@ -2,31 +2,53 @@ module Kanji where
 
 import Prelude
 
+import Bootstrap (col_, fluid, row_)
+import CSS (paddingTop, pct)
 import Data.Lens.Record (prop)
+import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
+import Fosskers.Kanji (Analysis)
 import Halogen as H
 import Halogen.HTML as HH
+import Halogen.HTML.CSS as HC
 import Halogen.HTML.Events as HE
-import Types (Effects, Language(..), defaultLang, update)
+import Halogen.HTML.Properties as HP
+import Halogen.Query.HalogenM as HQ
+import ServerAPI (postKanji)
+import Types (Effects, Language, defaultLang, update)
 
 ---
 
-data Query a = LangChanged Language a
+data Query a = LangChanged Language a | Update String a
 
-type State = { language :: Language }
+type State = { language :: Language, result :: Maybe Analysis }
 
 component :: forall e. H.Component HH.HTML Query Language Void (Effects e)
-component = H.component { initialState: const { language: defaultLang }
+component = H.component { initialState: const { language: defaultLang, result: Nothing }
                         , render
                         , eval
                         , receiver: HE.input LangChanged }
 
 render :: State -> H.ComponentHTML Query
-render s = HH.text t
-  where t = case s.language of
-          English  -> "Kanji page!"
-          Japanese -> "漢字"
+render s = fluid [ HC.style <<< paddingTop $ pct 1.0 ]
+           [ row_
+             [ col_
+               [ HH.div [ HP.class_ (H.ClassName "input-group") ]
+                 [ HH.div [ HP.class_ (H.ClassName "input-group-prepend") ]
+                   [ HH.span [ HP.class_ (H.ClassName "input-group-text") ] [ HH.text "Input" ]]
+                 , HH.textarea [ HP.class_ (H.ClassName "form-control")
+                               , HP.attr (H.AttrName "aria-label") "Input"
+                               , HE.onValueInput (\str -> Just $ Update str unit) ]
+                 ]
+               ]
+             ]
+           ]
 
-eval :: forall m. Query ~> H.ComponentDSL State Query Void m
+eval :: forall e. Query ~> H.ComponentDSL State Query Void (Effects e)
 eval = case _ of
   LangChanged l next -> update (prop (SProxy :: SProxy "language")) l *> pure next
+  Update s next -> do
+    _ <- HQ.fork do
+      a <- H.lift $ postKanji s
+      H.modify (_ { result = Just a })
+    pure next
--- site/test/Main.purs
+++ site/test/Main.purs
@@ -2,15 +2,16 @@ module Test.Main where
 
 import Prelude
 
-import Common (Path(..))
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
+import Fosskers.Common (Path(..))
+import Servant.PureScript.Settings (SPSettingsDecodeJson_(..), SPSettingsEncodeJson_(..), SPSettings_(..))
 import Test.Unit (suite, test)
 import Test.Unit.Assert as Assert
 import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
-import Types (Language(..), postLang)
+import Types (Language(..), postLang, settings)
 
 ---
 
@@ -20,9 +21,20 @@ main = runTest do
     test "Language Detection" do
       Assert.assert "deploying-haskell-en should be detected as English"  $ postLang epath == English
       Assert.assert "deploying-haskell-jp should be detected as Japanese" $ postLang jpath == Japanese
+    -- test "JSON Codec" do
+    --   Assert.assert "KText Isomorphism" $ Right ktext == decoder (encoder ktext)
 
 epath :: { path :: Path }
 epath = { path: Path "deploying-haskell-en" }
 
 jpath :: { path :: Path }
 jpath = { path: Path "deploying-haskell-jp" }
+
+opts = case settings of
+  SPSettings_ o -> o
+
+encoder = case opts.encodeJson of
+  SPSettingsEncodeJson_ e -> e
+
+decoder = case opts.decodeJson of
+  SPSettingsDecodeJson_ e -> e
