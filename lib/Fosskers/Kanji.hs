1eb4a86e4b83a14472a245cf23bd052f79d5dfcf
Colin Woodbury <colingw@gmail.com>
2018-03-18 16:47:12 -0700

    (server) `/kanji` endpoint returns more data

--- lib/Fosskers/Kanji.hs
+++ lib/Fosskers/Kanji.hs
@@ -13,13 +13,16 @@ import qualified Data.Text as T
 
 data Analysis = Analysis { levelSplit    :: [(Level, [Kanji])]  -- ^ Unique `Kanji` that appeared in each `Level`.
                          , elementary    :: Float  -- ^ Percent (<= 1.0) of `Kanji` learned in Elementary school.
+                         , middle        :: Float  -- ^ Percent (<= 1.0) of `Kanji` learned by the end of Middle School.
+                         , high          :: Float  -- ^ Percent (<= 1.0) of `Kanji` learned by the end of High School.
+                         , adult         :: Float  -- ^ Percent (<= 1.0) of `Kanji` that an adult should be able to read.
                          , density       :: Maybe Float  -- ^ Fraction (<= 1.0) of the input that was `Kanji`.
                          , distributions :: [(Level, Float)]  -- ^ Fractions of input `Kanji` that belong to each `Level`.
                          } deriving (Generic, ToJSON)
 
 analysis :: Text -> Analysis
-analysis t | T.null t = Analysis [] 0 Nothing []
-           | otherwise = Analysis uniq (elementaryDen dist) den (M.toList dist)
+analysis t | T.null t = Analysis [] 0 0 0 0 Nothing []
+           | otherwise = Analysis uniq (elementaryDen dist) (middleDen dist) (highDen dist) (adultDen dist) den (M.toList dist)
   where ks   = mapMaybe kanji $ unpack t
         uniq = M.toList . fmap S.toList $ uniques ks
         dist = levelDist ks
