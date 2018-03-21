ee68dbc4df0037abe456416f7d16ca446eb165d2
Colin Woodbury <colingw@gmail.com>
2018-03-21 10:51:21 -0700

    (site) Kanji demo now available in Japanese

--- site/src/Kanji.purs
+++ site/src/Kanji.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Bootstrap (col_, container, row, row_)
 import CSS (paddingTop, pct)
+import Control.Error.Util (bool)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Class (class MonadAff)
 import Control.Monad.Eff.Exception (EXCEPTION)
@@ -13,7 +14,7 @@ import Data.Array (null)
 import Data.Formatter.Number (Formatter(..), format)
 import Data.Generic (gShow)
 import Data.Int (ceil)
-import Data.Kanji.Types (CharCat(Other, Punctuation, RomanLetter, Numeral, Katakana, Hiragana, Hanzi), Kanji(Kanji))
+import Data.Kanji.Types as K
 import Data.Lens ((^.))
 import Data.Lens.Record (prop)
 import Data.Maybe (Maybe(Just, Nothing), maybe)
@@ -36,7 +37,7 @@ import Halogen.HTML.Properties as HP
 import Halogen.Query.HalogenM as HQ
 import Network.HTTP.Affjax (AJAX)
 import ServerAPI (getKanjiByText, postKanji)
-import Types (Effects, Effects', Four(..), Language, defaultLang, update)
+import Types (Effects, Effects', Four(..), Language(..), defaultLang, update)
 
 ---
 
@@ -57,8 +58,8 @@ component = H.parentComponent { initialState: const { language: defaultLang, ana
 
 render :: forall e. State -> H.ParentHTML Query EC.EChartsQuery Slot (Effects e)
 render s = container [ HC.style <<< paddingTop $ pct 1.0 ]
-           $ preamble <> buttons <> input <> maybe [] withResults s.analysis
-  where preamble =
+           $ preamble s.language <> buttons s.language <> note s.language <> input <> maybe [] withResults s.analysis
+  where preamble English =
           [ row_ [ col_
                    [ HH.p_ [ HH.text "This is a demo of my "
                            , HH.a [ HP.href "http://hackage.haskell.org/package/kanji" ] [ HH.text "Kanji" ]
@@ -74,16 +75,35 @@ render s = container [ HC.style <<< paddingTop $ pct 1.0 ]
                    , HH.p_ [ HH.text $ "Try entering text below, or select one of the sample texts"
                              <> " to see how widely each Kanji level is used in real life."
                            ]]]]
-        buttons = [ row_ [ col_
-                           [ HH.div [ HP.class_ (H.ClassName "btn-toolbar")
-                                    , HP.attr (H.AttrName "role") "toolbar"
-                                    , HP.attr (H.AttrName "aria-label") "Sample Texts" ]
-                             $ map button
-                             [ Four "First Group" "primary" "doraemon" "Wikipedia: Doraemon"
-                             , Four "Second Group" "warning" "sumo" "News: Asashoryu vs Hakoho"
-                             , Four "Third Group" "success" "rashomon" "Short Story: Rashomon (1915)"
-                             , Four "Last Group" "info" "iamacat" "Novel: I am a Cat (1905)"
-                             ]]]]
+        preamble Japanese =
+          [ row_ [ col_
+                   [ HH.p_ [ HH.text "これは私の"
+                           , HH.a [ HP.href "http://hackage.haskell.org/package/kanji" ] [ HH.text "「Kanji」" ]
+                           , HH.text "というHaskellライブラリのデモです。日本語を入力すると字が種別に、漢字が"
+                           , HH.a [ HP.href "http://www.kanken.or.jp" ] [ HH.text "日本漢字能力検定協会" ]
+                           , HH.text "が定義する「級」に分けられます。"
+                           ]
+                   , HH.p_ [ HH.text $ "欄に日本語を直接打ったり貼ったりするか、下の例文ボタンを押して"
+                             <> "どの級の漢字が実際にどれ程使われるかご覧ください。"
+                           ]
+                   ]
+                 ]]
+        buttons l =
+          [ row_
+            [ col_
+              [ HH.div [ HP.class_ (H.ClassName "btn-toolbar")
+                       , HP.attr (H.AttrName "role") "toolbar"
+                       , HP.attr (H.AttrName "aria-label") "Sample Texts" ]
+                $ map button
+                [ Four "First Group" "primary" "doraemon"
+                  $ bool "ウィキペディア・ドラえもん" "Wikipedia: Doraemon" (l == English)
+                , Four "Second Group" "warning" "sumo"
+                  $ bool "ニュース・朝青龍対白鵬" "News: Asashoryu vs Hakoho" (l == English)
+                , Four "Third Group" "success" "rashomon"
+                  $ bool "短篇小説・羅生門" "Short Story: Rashomon (1915)" (l == English)
+                , Four "Last Group" "info" "iamacat"
+                  $ bool "小説・吾輩は猫である" "Novel: I am a Cat (1905)" (l == English)
+                ]]]]
         button (Four a c f t) = HH.div [ HP.classes $ map H.ClassName ["btn-group", "mr-2"]
                                        , HP.attr (H.AttrName "role") "group"
                                        , HP.attr (H.AttrName "aria-label") a ]
@@ -92,20 +112,23 @@ render s = container [ HC.style <<< paddingTop $ pct 1.0 ]
                                            , HE.onClick (HE.input_ $ ButtonSelected f)
                                            ]
                                  [ HH.text t ]]
-        input = [ row [ HC.style <<< paddingTop $ pct 1.0 ]
+        note English  = [ row [ HC.style <<< paddingTop $ pct 1.0] [ col_ [ HH.p_ [ HH.i_ [ HH.text "Note: Kanji of level \"PreOne\" and above will appear as \"Unknown\". "]]]]]
+        note Japanese = [ row [ HC.style <<< paddingTop $ pct 1.0] [ col_ [ HH.p_ [ HH.i_ [ HH.text "注意：準一級以上の漢字は「未確定」と示される。"]]]]]
+        input = [ row_
                   [ col_
                     [ HH.div [ HP.class_ (H.ClassName "input-group") ]
                       [ HH.div [ HP.class_ (H.ClassName "input-group-prepend") ]
-                        [ HH.span [ HP.class_ (H.ClassName "input-group-text") ] [ HH.text "Input" ]]
+                        [ HH.span [ HP.class_ (H.ClassName "input-group-text") ]
+                          [ HH.text $ bool "入力" "Input" (s.language == English) ]]
                       , HH.textarea [ HP.class_ (H.ClassName "form-control")
                                     , HP.attr (H.AttrName "aria-label") "Input"
                                     , HE.onValueInput (\str -> Just $ Update str unit) ]]]]]
         withResults (Analysis a) = charts a <> unknowns a
         charts a = [ row [ HC.style <<< paddingTop $ pct 1.0 ]
-                     [ col_ [ density $ a ^. prop (SProxy :: SProxy "density") ]]
+                     [ col_ [ density s.language $ a ^. prop (SProxy :: SProxy "density") ]]
                    , row [ HC.style <<< paddingTop $ pct 1.0 ] [ chart 0, chart 1 ]]
-        unknowns a | null a.unknowns = [] -- = case M.lookup Unknown $ M.fromFoldable a.levelSplit of
-                   | otherwise = [ HH.hr_ , HH.h5_ [ HH.text "Kanji of Unknown Level"], HH.div_ (map weblio a.unknowns) ]
+        unknowns a | null a.unknowns = []
+                   | otherwise = [ HH.hr_ , HH.h5_ [ HH.text "級が未確定の漢字"], HH.div_ (map weblio a.unknowns) ]
 
 chart :: forall t452 m e.
   MonadAff ( echarts :: ET.ECHARTS, dom :: DOM, avar :: AVAR, exception :: EXCEPTION, ref :: REF | e ) m
@@ -114,12 +137,12 @@ chart n = HH.div [ HP.classes $ map HH.ClassName [ "col-xs-12", "col-md-6" ]]
           [ HH.slot n (EC.echarts Nothing) ({ width: 500, height: 350 } /\ unit)
             (Just <<< H.action <<< HandleEChartsMsg) ]
 
-weblio :: forall t4 t5. Kanji -> HH.HTML t5 t4
-weblio (Kanji k) = HH.a [ HP.href $ "https://weblio.jp/content/" <> k', HP.attr (H.AttrName "style") "float: left;" ] [ HH.h3_ [ HH.text k' ]]
+weblio :: forall t4 t5. K.Kanji -> HH.HTML t5 t4
+weblio (K.Kanji k) = HH.a [ HP.href $ "https://weblio.jp/content/" <> k', HP.attr (H.AttrName "style") "float: left;" ] [ HH.h3_ [ HH.text k' ]]
   where k' = S.singleton k
 
-density :: forall t340 t341. Array (Tuple CharCat Number) -> HH.HTML t341 t340
-density d = HH.div [ HP.class_ $ H.ClassName "progress"
+density :: forall t340 t341. Language -> Array (Tuple K.CharCat Number) -> HH.HTML t341 t340
+density l d = HH.div [ HP.class_ $ H.ClassName "progress"
                    , HP.attr (H.AttrName "style") "height: 35px;" ] $
             map f d
   where f (Tuple c n) =
@@ -133,16 +156,19 @@ density d = HH.div [ HP.class_ $ H.ClassName "progress"
                     , HP.attr (H.AttrName "aria-valuemin") "0"
                     , HP.attr (H.AttrName "aria-valuemax") "100" ]
              [ HH.text $ v' <> "% " <> label ]
-        g Hanzi       = Tuple "bg-info" "Kanji"
-        g Hiragana    = Tuple "bg-success" "Hiragana"
-        g Katakana    = Tuple "bg-warning" "Katakana"
-        g Numeral     = Tuple "bg-primary" "Numbers"
-        g RomanLetter = Tuple "bg-info" "Alphabet"
-        g Punctuation = Tuple "bg-dark" "Punctuation"
-        g Other       = Tuple "bg-secondary" "Other"
+        g K.Hanzi       = Tuple "bg-info" labels.kanji
+        g K.Hiragana    = Tuple "bg-success" labels.hira
+        g K.Katakana    = Tuple "bg-warning" labels.kata
+        g K.Numeral     = Tuple "bg-primary" labels.nums
+        g K.RomanLetter = Tuple "bg-info" labels.abcs
+        g K.Punctuation = Tuple "bg-dark" labels.punc
+        g K.Other       = Tuple "bg-secondary" labels.other
         form = Formatter { comma: false, before: 1, after: 1, abbreviations: false, sign: false }
+        labels = case l of
+          English -> { kanji: "Kanji", hira: "Hiragana", kata: "Katakana", nums: "Numbers", abcs: "Alphabet", punc: "Punctuation", other: "Other"}
+          Japanese -> { kanji: "漢字", hira: "ひらがな", kata: "カタカナ", nums: "数字", abcs: "ローマ字", punc: "句読点", other: "その他" }
 
--- これは日本語串糞猫牛虎山羊森林一二三
+type Labels = { kanji :: String, hira :: String, kata :: String, nums :: String, abcs :: String, punc :: String, other :: String }
 
 eval :: forall e. Query ~> H.ParentDSL State Query EC.EChartsQuery Slot Void (Effects' ( ajax :: AJAX | e ))
 eval = case _ of
@@ -151,59 +177,87 @@ eval = case _ of
   Update s next -> do
     _ <- HQ.fork do
       a <- H.lift $ postKanji s
-      updateCharts a
+      l <- H.gets _.language
+      updateCharts l a
       H.modify (_ { analysis = Just a })
     pure next
-  HandleEChartsMsg EC.Initialized next ->
-    (H.gets _.analysis >>= maybe (pure unit) updateCharts) *> pure next
+  HandleEChartsMsg EC.Initialized next -> do
+    s <- H.get
+    maybe (pure unit) (updateCharts s.language) s.analysis
+    pure next
+-- (H.gets _.analysis >>= maybe (pure unit) updateCharts) *> pure next
   HandleEChartsMsg (EC.EventRaised evt) next -> pure next
   ButtonSelected t next -> do
     _ <- HQ.fork do
       a <- H.lift $ getKanjiByText t
-      maybe (pure unit) updateCharts a
+      l <- H.gets _.language
+      maybe (pure unit) (updateCharts l) a
       H.modify (_ { analysis = a })
     pure next
 
-updateCharts :: forall t800 t801 t804 t805. Analysis -> H.HalogenM t805 t804 EC.EChartsQuery Int t801 t800 Unit
-updateCharts a = when (hasRealData a) do
-  void $ H.query 0 $ H.action $ EC.Set $ interpret $ byLevel a
-  void $ H.query 1 $ H.action $ EC.Set $ interpret $ lifeStages a
+updateCharts :: forall t800 t801 t804 t805. Language -> Analysis -> H.HalogenM t805 t804 EC.EChartsQuery Int t801 t800 Unit
+updateCharts l a = when (hasRealData a) do
+  void $ H.query 0 $ H.action $ EC.Set $ interpret $ byLevel l a
+  void $ H.query 1 $ H.action $ EC.Set $ interpret $ lifeStages l a
 
 hasRealData :: Analysis -> Boolean
 hasRealData (Analysis a) = not $ null a.distributions
 
-byLevel :: Analysis -> DSL' ETP.OptionI
-byLevel (Analysis a) = do
+byLevel :: Language -> Analysis -> DSL' ETP.OptionI
+byLevel l (Analysis a) = do
   E.tooltip do
     E.trigger ET.ItemTrigger
     E.formatterString "{b} <br /> {d}%"
   E.title do
-    E.text "Level Distributions"
-    E.subtext "Percentage of Kanji per Level"
+    E.text $ bool "級率" "Level Distributions" (l == English)
+    E.subtext $ bool "文はどの級の漢字でできたか" "Percentage of Kanji per Level" (l == English)
   E.series do
     E.pie do
       E.name "Levels"
       E.center $ ET.Point { x: ET.Percent 50.0, y: ET.Percent 60.0 }
       E.selectedMode ET.Single
       E.buildItems $
-        traverse_ (\(Tuple l n) -> E.addItem $ E.value n *> E.name (S.drop 17 $ gShow l)) a.distributions
+        traverse_ (\(Tuple le n) -> E.addItem $ E.value n *> E.name (label l le)) a.distributions
+  where label English le  = S.drop 17 $ gShow le
+        label Japanese le = levelJ le
 
-lifeStages :: Analysis -> DSL' ETP.OptionI
-lifeStages (Analysis a) = do
+levelJ :: K.Level -> String
+levelJ K.Ten = "十級"
+levelJ K.Nine = "九級"
+levelJ K.Eight = "八級"
+levelJ K.Seven = "七級"
+levelJ K.Six = "六級"
+levelJ K.Five = "五級"
+levelJ K.Four = "四級"
+levelJ K.Three = "三級"
+levelJ K.PreTwo = "準二級"
+levelJ K.Two = "二級"
+levelJ K.PreOne = "準一級"
+levelJ K.One = "一級"
+levelJ K.Unknown = "未確定"
+
+lifeStages :: Language -> Analysis -> DSL' ETP.OptionI
+lifeStages l (Analysis a) = do
   E.tooltip do
     E.trigger ET.ItemTrigger
     E.formatterString "{b} <br /> {d}%"
   E.title do
-    E.text "Kanji by Life Stages"
-    E.subtext "Percentage of Kanji learned by different life stages"
+    E.text $ bool "人生段階率" "Kanji by Life Stages" (l == English)
+    E.subtext
+      $ bool "人生のどの段階までにどの級の漢字を学習しているはずか" "Percentage of Kanji learned by different life stages" (l == English)
   E.series do
     E.pie do
       E.name "Kanji"
       E.center $ ET.Point { x: ET.Percent 50.0, y: ET.Percent 60.0 }
       E.selectedMode ET.Single
       E.buildItems do
-        E.addItem $ E.value a.elementary *> E.name "Elementary Shcool"
-        E.addItem $ E.value (a.middle - a.elementary) *> E.name "Middle School"
-        E.addItem $ E.value (a.high - a.middle) *> E.name "High School"
-        E.addItem $ E.value (a.adult - a.high) *> E.name "Adult"
-        E.addItem $ E.value (1.0 - a.adult) *> E.name "Higher"
+        E.addItem $ E.value a.elementary *> E.name labels.elem
+        E.addItem $ E.value (a.middle - a.elementary) *> E.name labels.mid
+        E.addItem $ E.value (a.high - a.middle) *> E.name labels.high
+        E.addItem $ E.value (a.adult - a.high) *> E.name labels.adult
+        E.addItem $ E.value (1.0 - a.adult) *> E.name labels.above
+  where labels = case l of
+          English  -> { elem: "Elementary School", mid: "Middle School", high: "High School", adult: "Adulthood", above: "Higher" }
+          Japanese -> { elem: "小学校", mid: "中学校", high: "高校", adult: "大学・一般人", above: "その上" }
+
+type LifeStageLabels = { elem :: String, mid :: String, high :: String, adult :: String, above :: String }
