f07ee1ac7ac327a110096e9972b35322150c958e
Colin Woodbury <colin@fosskers.ca>
2019-11-18 17:47:36 -0800

    Last lint

--- lib/Fosskers/Org.hs
+++ lib/Fosskers/Org.hs
@@ -21,10 +21,10 @@ title = Title . T.pack <$> (string "#+TITLE: " *> someTill anySingle newline)
 date :: Parser Date
 date = do
   void $ string "#+DATE: "
-  year  <- L.decimal <* char '-'
-  month <- fmap (toEnum . pred) L.decimal <* char '-'
-  day   <- L.decimal
-  pure $ Date year month day
+  Date
+    <$> (L.decimal <* char '-')
+    <*> (fmap (toEnum . pred) L.decimal <* char '-')
+    <*> L.decimal
 
 parseOrg :: T.Text -> T.Text -> Either T.Text (Title, Date)
 parseOrg fp t = first (T.pack . errorBundlePretty) $ parse org (T.unpack fp) t
