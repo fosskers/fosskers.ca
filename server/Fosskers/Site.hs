d36e90d9b6d00dfbba86383ee58974d3f3bcd994
Colin Woodbury <colin@fosskers.ca>
2020-02-09 23:03:48 -0800

    Simple navbar
    Recreated without too much effort by referencing the Purescript as well
    as the official Bootstrap docs.

--- server/Fosskers/Site.hs
+++ server/Fosskers/Site.hs
@@ -1,8 +1,9 @@
-module Fosskers.Site where
+module Fosskers.Site ( index ) where
 
-import BasePrelude
+import BasePrelude hiding (index)
 import Data.Text (Text)
 import Lucid
+import Lucid.Base (makeAttribute)
 
 ---
 
@@ -21,11 +22,11 @@ index = html_ $ head_ h *> body_ (topbar *> div_ "Real content here.")
     h = do
       title_ "fosskers.ca"
       meta_ [ charset_ "utf-8" ]
-      -- meta_ [ name_ "viewport", content_ "width=device-width, initial-scale=1, shrink-to-fit=no" ]
-      -- script_ [ src_ "assets/jquery.slim.min.js" ] ("" :: Text)
-      -- script_ [ src_ "assets/bootstrap.min.js" ] ("" :: Text)
-      -- link_ [ rel_ "stylesheet"
-      --       , href_ "assets/bootstrap.min.css" ]
+      meta_ [ name_ "viewport", content_ "width=device-width, initial-scale=1, shrink-to-fit=no" ]
+      script_ [ src_ "assets/jquery.slim.min.js" ] ("" :: Text)
+      script_ [ src_ "assets/bootstrap.min.js" ] ("" :: Text)
+      link_ [ rel_ "stylesheet"
+            , href_ "assets/bootstrap.min.css" ]
       -- link_ [ rel_ "stylesheet"
       --       , href_ "assets/fontawesome.min.css" ]
       -- link_ [ rel_ "stylesheet"
@@ -36,13 +37,43 @@ index = html_ $ head_ h *> body_ (topbar *> div_ "Real content here.")
       --       , href_ "assets/fosskers.css" ]
 
 topbar :: Html ()
-topbar = div_ $ do
-  "About"
-  "Tools"
-  "Blog"
-  "CV"
-  "RSS"
-  "Email / Social Links"
+topbar = nav_ [ classes_ [ "navbar", "navbar-expand-lg", "navbar-dark", "bg-dark" ] ] $ do
+  a_ [ class_ "navbar-brand", href_ "#" ] "Fosskers"
+  navButton
+  div_ [ classes_ [ "collapse", "navbar-collapse" ], id_ navId ] $
+    ul_ [ class_ "navbar-nav" ] $ do
+      item "About"
+      item "Blog"
+      li_ [ classes_ [ "nav-item", "dropdown" ] ] $ do
+        a_ [ classes_ [ "nav-link", "dropdown-toggle" ]
+           , href_ "#"
+           , id_ "navbarDropdown"
+           , role_ "button"
+           , makeAttribute "data-toggle" "dropdown"
+           , makeAttribute "aria-haspopup" "true"
+           , makeAttribute "aria-expanded" "false" ] "Projects"
+        div_ [ class_ "dropdown-menu"
+             , makeAttribute "aria-labelledby" "navbarDropdown" ] $ do
+          a_ [ class_ "dropdown-item", href_ "#" ] "Aura"
+          a_ [ class_ "dropdown-item", href_ "#" ] "Bag of Holding"
+          a_ [ class_ "dropdown-item", href_ "#" ] "Foobar"
+      item "Tools"
+      item "CV"
+  where
+    navId :: Text
+    navId = "navbarLinks"
 
-blog :: Text -> Html ()
-blog = undefined
+    navButton :: Html ()
+    navButton = button_
+      [ class_ "navbar-toggler"
+      , type_ "button"
+      , makeAttribute "data-toggle" "collapse"
+      , makeAttribute "data-target" $ "#" <> navId
+      , makeAttribute "aria-controls" navId
+      , makeAttribute "aria-expanded" "false"
+      , makeAttribute "aria-label" "Toggle navigation" ]
+      $ span_ [ class_ "navbar-toggler-icon" ] ""
+
+    item :: Html () -> Html ()
+    item l = li_ [ class_ "nav-item" ] $
+      a_ [ class_ "nav-link", href_ "#" ] l
