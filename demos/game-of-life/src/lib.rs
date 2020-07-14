7eb37a616aff71aa970e862c480ae1bd9be6ce09
Colin Woodbury <colin@fosskers.ca>
2020-07-13 18:58:43 -0700

    [demo] Can manipulate a <canvas> from Rust

--- demos/game-of-life/src/lib.rs
+++ demos/game-of-life/src/lib.rs
@@ -1,5 +1,8 @@
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use web_sys::console;
+
+const PI: f64 = 3.1415683859;
 
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -7,6 +10,27 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 #[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
     console::log_1(&"Hello from Rust!".into());
+
+    let document = web_sys::window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id("smile-canvas").unwrap();
+    let canvas: web_sys::HtmlCanvasElement = canvas
+        // .dyn_into::<web_sys::HtmlCanvasElement>()
+        .dyn_into()
+        .map_err(|_| ())
+        .unwrap();
+
+    let context: web_sys::CanvasRenderingContext2d = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into()
+        .unwrap();
+
+    context.begin_path();
+
+    context.arc(75.0, 75.0, 50.0, 0.0, PI * 2.0).unwrap();
+
+    context.stroke();
 
     Ok(())
 }
--- server/Fosskers/Site/GameOfLife.hs
+++ server/Fosskers/Site/GameOfLife.hs
@@ -9,7 +9,12 @@ import Lucid.Base (termRawWith)
 -- This WASM loading magic is borrowed from:
 -- https://rustwasm.github.io/docs/wasm-bindgen/examples/without-a-bundler.html
 gol :: Language -> Html ()
-gol _ = termRawWith "script" [type_ "module"]
+gol _ = do
+  canvas_ [id_ "smile-canvas"] ""
+  wasm
+
+wasm :: Html ()
+wasm = termRawWith "script" [type_ "module"]
   "import init from '/assets/game_of_life.js';\n\
   \async function run() {\n\
   \  await init();\n\
