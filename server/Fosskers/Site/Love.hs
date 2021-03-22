6c6e3ef552d2a76a3150d5a2625e3df3e5a35c6f
Colin Woodbury <colin@fosskers.ca>
2021-03-21 20:07:17 -0700

    [loveletter] Move assets over

---
+++ server/Fosskers/Site/Love.hs
@@ -0,0 +1,26 @@
+module Fosskers.Site.Love ( love ) where
+
+import BasePrelude
+import Lucid
+import Lucid.Base
+
+---
+
+love :: Html ()
+love = html_ $ do
+  head_ h
+  body_ $ do
+    section_ [id_ "app"] ""
+    wasm
+  where
+    h :: Html ()
+    h = do
+      title_ "Love Letter Tracker"
+      meta_ [charset_ "utf-8"]
+      meta_ [name_ "viewport", content_ "width=device-width, initial-scale=1, shrink-to-fit=no"]
+      link_ [rel_ "stylesheet", type_ "text/css", href_ "/assets/love.css"]
+
+wasm :: Html ()
+wasm = termRawWith "script" [type_ "module"]
+  "import init from '/assets/love.js';\n\
+  \init('/assets/love_bg.wasm');"
