98f0f28d694176fd05b260e85eb7e22a0a89784f
Colin Woodbury <colin@fosskers.ca>
2021-04-07 21:31:03 -0700

    [demos] Game of Life: Fixed layout

--- server/Fosskers/Site/GameOfLife.hs
+++ server/Fosskers/Site/GameOfLife.hs
@@ -10,35 +10,33 @@ import Lucid.Base (termRawWith)
 -- This WASM loading magic is borrowed from:
 -- https://rustwasm.github.io/docs/wasm-bindgen/examples/without-a-bundler.html
 gol :: Language -> Html ()
-gol lang = do
+gol lang = div_ [class_ "grid-main"] $ div_ [class_ "game-of-life"] $ do
   case lang of
     English  -> english
     Japanese -> japanese
-  div_ [class_ "title", style_ "padding-bottom: 1%"] $
-    div_ [class_ "btn-group", role_ "group"] $ do
-      button_ [type_ "button", classes_ ["btn", "btn-outline-warning"], id_ "pause-button"] "Pause"
-      button_ [type_ "button", classes_ ["btn", "btn-outline-dark"], id_ "reset-button"] "Reset"
-  div_ [class_ "title"] $ do
-    canvas_ [id_ "game-of-life-canvas"] ""
-    wasm
+  div_ [classes_ ["buttons", "has-addons"]] $ do
+      button_ [classes_ ["button", "is-outlined", "is-warning"], id_ "pause-button"] "Pause"
+      button_ [classes_ ["button", "is-outlined", "is-dark"], id_ "reset-button"] "Reset"
+  canvas_ [id_ "game-of-life-canvas"] ""
+  wasm
   where
     english :: Html ()
     english = do
-      div_ [class_ "title"] $ do
+      p_ $ do
         "This is the "
         a_ [href_ "https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"] "Game of Life"
         " written entirely in "
         a_ [href_ "https://www.rust-lang.org/"] "Rust"
         " with its "
         a_ [href_ "https://rustwasm.github.io/docs/book/"] "support for Web Assembly."
-      div_ [class_ "title", style_ "padding-bottom: 1%"] $ do
+      p_ $ do
         "The grid itself is an HTML5 "
         code_ "<canvas>"
         " element written to directly from Rust."
 
     japanese :: Html ()
     japanese = do
-      div_ [class_ "title"] $ do
+      p_ $ do
         "Conwayの"
         a_ [href_ "https://ja.wikipedia.org/wiki/%E3%83%A9%E3%82%A4%E3%83%95%E3%82%B2%E3%83%BC%E3%83%A0"] "ライフゲーム"
         "です。コードはJavascriptを一切使わずに全て"
@@ -46,7 +44,7 @@ gol lang = do
         "とその"
         a_ [href_ "https://rustwasm.github.io/docs/book/"] "Web Assemblyバックエンド"
         "で作られています。"
-      div_ [class_ "title", style_ "padding-bottom: 1%"] $ do
+      p_ $ do
         "盤自体はRustから直接書き込むHTML5の"
         code_ "<canvas>"
         "です。"
