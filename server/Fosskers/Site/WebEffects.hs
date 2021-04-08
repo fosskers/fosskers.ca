7285a567cbe425ccde4bc4c88ee9b995a63ba418
Colin Woodbury <colin@fosskers.ca>
2021-04-08 13:19:01 -0700

    [demo] Print dialog works

--- server/Fosskers/Site/WebEffects.hs
+++ server/Fosskers/Site/WebEffects.hs
@@ -12,12 +12,12 @@ webEffects lang = div_ [class_ "grid-main"] $ div_ [class_ "web-effects"] $ do
   h1_ [classes_ ["title", "is-1"]] "Web Effects"
   p_ [classes_ ["subtitle", "is-5"]] "Various browser interaction samples."
   div_ $ do
-    button_ [class_ "button"] "Print this Page"
-  -- wasm
+    button_ [class_ "button", id_ "print-button"] "Print this Page"
+  wasm
 
 wasm :: Html ()
 wasm = termRawWith "script" [type_ "module"]
-  "import init from '/assets/code/game_of_life.js';\n\
+  "import init from '/assets/code/web_effects.js';\n\
   \async function run() {\n\
   \  await init();\n\
   \}\n\
